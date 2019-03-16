28e8fbe693d0a247e13ec90d3e570a42f5c75136
Quy Tran <quytran288@gmail.com>
2019-03-16 11:30:51 -0700

    First commit with some unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+import App from './App';
+import Calculator from './Calculator/Calculator';
+
+describe('<App />', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+
+  it('should render correctly', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render a "div"', () => {
+    expect(wrapper.find('div').length).toEqual(1);
+  });
+
+  it('should render Calculator component', () => {
+    expect(wrapper.containsMatchingElement(<Calculator />)).toEqual(true);
+  });
+})
