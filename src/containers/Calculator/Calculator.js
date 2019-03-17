9fa90b912a276fc82757d9abc65bf95c575b74dc
Quy Tran <quytran288@gmail.com>
2019-03-17 01:03:25 -0700

    Unit test for updateDisplay function and draft version of updateDisplay function

--- src/containers/Calculator/Calculator.js
+++ src/containers/Calculator/Calculator.js
@@ -5,6 +5,10 @@ import Display from '../../components/Display/Display';
 import Keypad from '../../components/Keypad/Keypad';
 
 class Calculator extends Component {
+	constructor() {
+		super();
+		this.updateDisplay.bind(this);
+	}
 	state = {
 		displayValue: '0',
 		numbers: ['9', '8', '7', '6', '5', '4', '3', '2', '1', '.', '0','ce'],
@@ -21,8 +25,30 @@ class Calculator extends Component {
 		console.log('set operation');
 	}
 
-	updateDisplay = () => {
-		console.log('update display');
+	updateDisplay = (val) => {
+		let {displayValue} = this.state;
+
+		if (val === 'ce') {
+			if (displayValue.length <= 1) {
+				displayValue = '0';
+			} else {
+				displayValue = displayValue.slice(0, displayValue.length - 1);
+			}
+		} else if(val === '.') {
+			if (displayValue === '0') {
+				displayValue = '.';
+			} else {
+				displayValue += displayValue.includes('.') ? '' : val;
+			}
+		} else {
+			if (displayValue === '0') {
+				displayValue = val;
+			} else {
+				displayValue += val;
+			}
+		}
+
+		this.setState({displayValue});
 	}
 
 	render = () => {
--- src/containers/Calculator/Calculator.test.js
+++ src/containers/Calculator/Calculator.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 import Calculator from './Calculator';
 import Display from '../../components/Display/Display';
@@ -35,5 +35,95 @@ describe('<Calculator />', () => {
 				/>
 			]
 		)).toEqual(true);
+	});
+});
+
+describe('mounted <Calculator />', () => {
+	let wrapper;
+
+	beforeEach(() => {
+		wrapper = mount(<Calculator/>);
+	});
+
+	it('should calls updateDisplay when a number key is clicked', () => {
+		const spy = jest.spyOn(wrapper.instance(), 'updateDisplay');
+		wrapper.instance().forceUpdate();
+		expect(spy).toHaveBeenCalledTimes(0);
+		wrapper.find('.number-key').first().simulate('click');
+		expect(spy).toHaveBeenCalledTimes(1);
+	});
+
+	it('should calls setOperator when a operator key is clicked', () => {
+		const spy = jest.spyOn(wrapper.instance(), 'setOperator');
+		wrapper.instance().forceUpdate();
+		expect(spy).toHaveBeenCalledTimes(0);
+		wrapper.find('.operator-key').first().simulate('click');
+		expect(spy).toHaveBeenCalledTimes(1);
+	});
+
+	it('should calls callOperator operator when submit key is clicked', () => {
+		const spy = jest.spyOn(wrapper.instance(), 'callOperator');
+		wrapper.instance().forceUpdate();
+		expect(spy).toHaveBeenCalledTimes(0);
+		wrapper.find('.submit-key').first().simulate('click');
+		expect(spy).toHaveBeenCalledTimes(1);
+	});
+});
+
+describe('updateDisplay', () => {
+	let wrapper, wrapperInstance;
+
+	beforeEach(() => {
+		wrapper = shallow(<Calculator/>);
+		wrapperInstance = wrapper.instance();
+	});
+
+	it('should updates displayValue', () => {
+		wrapperInstance.updateDisplay('8');
+		expect(wrapperInstance.state.displayValue).toEqual('8');
+	});
+
+	it('should concatenates displayValue', () => {
+		wrapperInstance.updateDisplay('2');
+		wrapperInstance.updateDisplay('7');
+		expect(wrapperInstance.state.displayValue).toEqual('27');
+	});
+
+	it('should removes leading "0" from displayValue', () => {
+		wrapperInstance.updateDisplay('0');
+		wrapperInstance.updateDisplay('8');
+		expect(wrapperInstance.state.displayValue).toEqual('8');
+	});
+
+	it('should prevents multiple leading "0"s from displayValue', () => {
+		wrapperInstance.updateDisplay('0');
+		wrapperInstance.updateDisplay('0');
+		expect(wrapperInstance.state.displayValue).toEqual('0');
+	});
+
+	it('should prevent continuous multiple "." from displayValue', () => {
+		wrapperInstance.updateDisplay('.');
+		wrapperInstance.updateDisplay('.');
+		expect(wrapperInstance.state.displayValue).toEqual('.');
+	});
+
+	it('should prevent multiple "." from displayValue', () => {
+		wrapperInstance.updateDisplay('1');
+		wrapperInstance.updateDisplay('.');
+		wrapperInstance.updateDisplay('2');
+		wrapperInstance.updateDisplay('.');
+		expect(wrapperInstance.state.displayValue).toEqual('1.2');
+	});
+
+	it('should remove last number from displayValue', () => {
+		wrapperInstance.updateDisplay('1');
+		wrapperInstance.updateDisplay('9');
+		wrapperInstance.updateDisplay('ce');
+		expect(wrapperInstance.state.displayValue).toEqual('1');
+	});
+
+	it('should display 0 when there is only 1 number left in displayValue', () => {
+		wrapperInstance.updateDisplay('ce');
+		expect(wrapperInstance.state.displayValue).toEqual('0');
 	});
 });
